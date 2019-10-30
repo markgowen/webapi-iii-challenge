daf5f15f94c08b9a54c755fc4aba466577fb0fd7
Mark Gowen <mark.gowen78@gmail.com>
2019-10-30 13:00:25 -0700

    Updated server and more
    updated server.js with logger middleware
    added dependencies
    connected index.js to server

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const express = 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
